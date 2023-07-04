d6d4ae0bdc6570d647464178758a4fe80d2b00bf
harold <harold@pwrdrvr.com>
2023-07-04 14:54:12 -0400

    Better example - doesn't work yet

--- examples/express.ts
+++ examples/express.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import session, { SessionOptions } from 'express-session';
+import session from 'express-session';
 import { DynamoDBStore } from '@pwrdrvr/connect-dynamodb-v3';
 import * as dynamodb from '@aws-sdk/client-dynamodb';
 import express from 'express';
@@ -8,36 +8,40 @@ const { TABLE_NAME = 'connect-dynamodb-v3-test', PORT = '3001' } = process.env;
 
 const dynamoDBClient = new dynamodb.DynamoDBClient({});
 
-const sessionConfig: SessionOptions = {
-  store: new DynamoDBStore({
-    tableName: TABLE_NAME,
-    dynamoDBClient,
-  }),
-  secret: 'yeah-dont-use-this',
-  cookie: {
-    maxAge: 60 * 60 * 1000, // one hour in milliseconds
-    sameSite: 'none',
-    secure: true,
-  },
-  // We implement `touch` to update the TTL on the session store
-  // We do not want unmodified sessions to be saved as that will cause a
-  // potentially massive cost issue on DynamoDB
-  resave: false,
-  saveUninitialized: false,
-};
-
 const app = express();
 const port = 3001;
 
+app.use(
+  session({
+    store: new DynamoDBStore({
+      tableName: TABLE_NAME,
+      dynamoDBClient,
+    }),
+    secret: 'yeah-dont-use-this',
+    cookie: {
+      maxAge: 60 * 60 * 1000, // one hour in milliseconds
+      sameSite: 'none',
+      secure: true,
+    },
+    // We implement `touch` to update the TTL on the session store
+    // We do not want unmodified sessions to be saved as that will cause a
+    // potentially massive cost issue on DynamoDB
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+// Add a fake login route that will set a session cookie
+app.get('/login', (req, res) => {
+  console.log(`Session ID: ${req.session?.id}`);
+  res.send('Logged in');
+});
+
 // Return a 200 response for all routes
-app.use((req, res, next) => {
-  res.send('Hello World!');
-  res.status(200);
-  next();
+app.get('/*', (req, res) => {
+  res.status(200).send('Hello world');
 });
 
 app.listen(Number.parseInt(PORT, 10), () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-app.use(session(sessionConfig));
