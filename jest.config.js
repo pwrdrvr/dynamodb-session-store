7f11f8d8dafcb987e373bdcbc7e752fed45965e1
Harold Hunt <hhunt@giphy.com>
2025-02-19 15:13:57 -0500

    Add coverage report

--- jest.config.js
+++ jest.config.js
@@ -42,7 +42,7 @@ module.exports = {
   //   "lcov",
   //   "clover"
   // ],
-  coverageReporters: ['html', 'text', 'clover'],
+  coverageReporters: ['lcov', 'html', 'text'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
